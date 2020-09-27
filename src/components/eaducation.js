91de652f0ee99702fb5910fb20cae3d7b90c67d5
= <=>
2020-09-27 20:21:48 +0530

    Eaducation Details Added

---
+++ src/components/eaducation.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+class eaducation extends React.Component {
+  render(){
+    return(
+        <div class="container">
+            <div>
+                <h1>
+                    10th Eaducation:-
+                </h1>
+                <div>
+                    Rajendra Vidyalaya <br/>
+                    ICSE School <br/>
+                    MArks: 92.1% 
+                </div>
+            </div>
+            <br></br>
+            <div>
+                <h1>
+                    12th Eaducation:-
+                </h1>
+                <div>
+                    Rajendra Vidyalaya(ICSE)<br></br>
+                    Marks: 86.7%
+                </div>
+            </div>
+        </div>
+
+    );
+  }
+}
+
+// ReactDOM.render(temp,document.getElementById("root"));
+export default eaducation;
