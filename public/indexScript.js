8d619314a27b06d598a5a1d08e6b7d326d253d0f
= <=>
2020-09-28 23:36:23 +0530

    Initial Window Transistion Completed

--- public/indexScript.js
+++
@@ -1,4 +0,0 @@
-const t = gsap.timeline({dafaults : {ease : "power1.out"}});
-console.log("Done");
-t.to('.textspan' , { y:"0%" , duration : 1});
-t.to('#helperSplashWindow',{y:"-100%" , duration :2});
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,11 @@ function Application() {
   useEffect( ()=> {
     const t = gsap.timeline({dafaults : {ease : "power1.out"}});
     console.log("Done");
-    t.to('.textspan' , { y:"0%" , duration : 1});
+    t.to('.textspan1' , { y:"0%" , duration : 0.5 , stagger:0.25});
+    t.to('.textspan2', {y:"0%" , duration : 0.5 , stager :0.25});
+    t.to('.helperSplashWindow', {y:"-100%" , duration :1.3 , delay : 0.5 });
+    t.to('.initialSplashWindow',{ y:"-100%" , duration:1.5} , "-=1");
+
   });
     return(
       <Router>
